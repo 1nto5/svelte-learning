e79b463a19c618b24956795400451b7bf1f6c400
Adrian <aantosiak@icloud.com>
2024-10-30 13:57:26 +0100

    first commit

---
+++ src/routes/post/+page.svelte
@@ -0,0 +1,21 @@
+<script lang="ts">
+	import { enhance } from '$app/forms';
+	import type { ActionData } from './$types';
+
+	let { form }: { form: ActionData } = $props();
+</script>
+
+<h1>Add post</h1>
+<form method="post" action="?/login" use:enhance>
+	<label>
+		Title
+		<input name="title" />
+	</label>
+	<label>
+		Content
+		<input name="content" />
+	</label>
+	<button>Save</button>
+	<!-- <button formaction="?/register">Register</button> -->
+</form>
+<p style="color: red">{form?.message ?? ''}</p>
